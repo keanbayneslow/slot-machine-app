7cb41fe20b4e8dfa90089a68444f2b42c9c3e322
Kean Bayneslow <kean.bayneslow@gmail.com>
2024-03-20 13:30:33 +1100

    adds initial logic to populate reels with symbols

--- project.js
+++ project.js
@@ -4,24 +4,18 @@ const ROWS = 3;
 const COLS = 3;
 
 const SYMBOLS_COUNT = {
-    "A": 2, 
-    "B": 4, 
-    "C": 6, 
-    "D": 8 
+    A: 2, 
+    B: 4, 
+    C: 6, 
+    D: 8 
 };
 
 const SYMBOL_VALUES = {
-    "A": 5, 
-    "B": 4, 
-    "C": 3, 
-    "D": 2 
+    A: 5, 
+    B: 4, 
+    C: 3, 
+    D: 2 
 };
-
-
-
-
-
-
 
 const deposit = () => {
   while (true) {
@@ -60,6 +54,27 @@ const getBet = (balance, lines) => {
       return numberBet;
     }
   }
+};
+
+const spin = () => {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for (let i = 0; i < count; i++) {
+            symbols.push(symbol);
+        }
+    }
+
+    const reels = [[], [], []];
+    for (let i = 0; i < COLS; i++) {
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++) {
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1); 
+        }
+    }
+    return reels;
 };
 
 let balance = deposit();
