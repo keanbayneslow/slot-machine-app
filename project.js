13335a9c83b0ee5b55a0e2f38bc6576171137aba
Kean Bayneslow <kean.bayneslow@gmail.com>
2024-03-20 13:08:29 +1100

    adds initial symbaol counts and values for payout mutiplyer

--- project.js
+++ project.js
@@ -1,5 +1,28 @@
 const prompt = require("prompt-sync")();
 
+const ROWS = 3; 
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    "A": 2, 
+    "B": 4, 
+    "C": 6, 
+    "D": 8 
+};
+
+const SYMBOL_VALUES = {
+    "A": 5, 
+    "B": 4, 
+    "C": 3, 
+    "D": 2 
+};
+
+
+
+
+
+
+
 const deposit = () => {
   while (true) {
     const depositAmount = prompt("Enter a deposit amount: ");
@@ -14,29 +37,29 @@ const deposit = () => {
 };
 
 const getNumberOfLines = () => {
-    while (true) {
-        const lines = prompt("Enter the number of lines for this bet (1-3): ");
-        const numberOfLines = parseFloat(lines);
-    
-        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
-          console.log("Invalid number of lines, try again");
-        } else {
-          return numberOfLines;
-        }
-      }  
+  while (true) {
+    const lines = prompt("Enter the number of lines for this bet (1-3): ");
+    const numberOfLines = parseFloat(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Invalid number of lines, try again");
+    } else {
+      return numberOfLines;
+    }
+  }
 };
 
 const getBet = (balance, lines) => {
-    while (true) {
-        const bet = prompt("Enter the bet per line: ");
-        const numberBet = parseFloat(bet);
-    
-        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
-          console.log("Invalid bet, try again");
-        } else {
-          return numberBet;
-        }
-      } 
+  while (true) {
+    const bet = prompt("Enter the bet per line: ");
+    const numberBet = parseFloat(bet);
+
+    if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+      console.log("Invalid bet, try again");
+    } else {
+      return numberBet;
+    }
+  }
 };
 
 let balance = deposit();
