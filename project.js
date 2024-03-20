529d62976e5cc20e63d04cd934d1edf658b45d83
Kean Bayneslow <kean.bayneslow@gmail.com>
2024-03-20 13:46:26 +1100

    adds logic to transpose matrices for gamified output

--- project.js
+++ project.js
@@ -64,8 +64,9 @@ const spin = () => {
         }
     }
 
-    const reels = [[], [], []];
+    const reels = [];
     for (let i = 0; i < COLS; i++) {
+        reels.push([]);
         const reelSymbols = [...symbols];
         for (let j = 0; j < ROWS; j++) {
             const randomIndex = Math.floor(Math.random() * reelSymbols.length);
@@ -77,6 +78,20 @@ const spin = () => {
     return reels;
 };
 
+const transpose = (reels) => {
+    const rows = [];
+
+    for (let i = 0; i < ROWS; i++) {
+        rows.push([]);
+        for (let j = 0; j < COLS; j++) {
+            rows[i].push(reels[j][i]);
+        }
+    }
+    return rows
+};
+
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
+const reels = spin();
+const rows = transpose(reels);
